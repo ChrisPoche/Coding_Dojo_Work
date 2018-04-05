0b6c6b8153e0adf8ddd67931f99b27f3c1908b5b
Chris Poche <capoche1@gmail.com>
2018-04-05 14:50:42 -0500

    First Node Server

---
+++ Mean/JS/landing_page/app.js
@@ -0,0 +1,31 @@
+var http = require('http');
+var fs = require('fs');
+var server = http.createServer(function(request, response){
+    console.log('Client Request URL:', request.url);
+    if(request.url==='/'){
+        fs.readFile('index.html','utf8', function(errors, contents){
+            response.writeHead(200, {'Content-Type': 'text/html'});
+            response.write(contents);
+            response.end();
+        });
+    }
+    else if(request.url === '/dojos/new'){
+        fs.readFile('dojos.html', 'utf8', function(errors, contents){
+            response.writeHead(200, {'Content-Type':'text/html'});
+            response.write(contents);
+            response.end();
+        });
+    }
+    else if(request.url === '/ninjas'){
+        fs.readFile('ninjas.html', 'utf8', function(errors, contents){
+            response.writeHead(200, {'Content-Type':'text/html'});
+            response.write(contents);
+            response.end();
+        });
+    }
+    else{
+        response.end('URL requested is not available');
+    }
+});
+server.listen(6789);
+console.log('Running on port 6789');
