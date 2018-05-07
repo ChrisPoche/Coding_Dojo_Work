9f902ce0346f91844d5bac0b730c3b50183ce6ad
Chris Poche <capoche1@gmail.com>
2018-05-07 16:13:14 -0500

    Simple mvc returning a json object pulled from url parameters

---
+++ CSharp/callingCard/Controllers/CallingCardController.cs
@@ -0,0 +1,27 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+ 
+namespace callingCard.Controllers
+{
+    public class CallingCardController : Controller
+    {
+        [HttpGet]
+        [Route("")]
+        public string Index(){
+            return "Here's the landing page, enter parameters in URL: localhost:5000/{FirstName}/{LastName}/{Age}/{Color}";
+        }
+
+
+        [HttpGet]
+        [Route("{FirstName}/{LastName}/{Age}/{Color}")]
+        public JsonResult DisplayResults(string FirstName, string LastName, int Age, string Color){
+            var UrlResults = new {
+                FirstName = FirstName,
+                LastName = LastName,
+                Age = Age,
+                Color = Color,
+            };
+            return Json(UrlResults);
+        }
+    }
+}
