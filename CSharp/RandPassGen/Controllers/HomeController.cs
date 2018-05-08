4e73056ba28b9d6bbceaf3c9ff9ad5a36cb521ee
Chris Poche <capoche1@gmail.com>
2018-05-08 14:57:45 -0500

    Random 14 char key generator, with session counter and Ajax reload

---
+++ CSharp/RandPassGen/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+
+namespace RandPassGen.Controllers
+{
+    public class HomeController : Controller
+    {
+        int passNum = 1; 
+        
+        [HttpGet]
+        [Route("")]
+        public IActionResult Index()
+        {   
+            if(HttpContext.Session.GetInt32("PassNum")== null){
+                HttpContext.Session.SetInt32("PassNum",passNum);
+            }
+            else{
+                int? plusUp = HttpContext.Session.GetInt32("PassNum")+1;
+                int Count = (int)plusUp;
+                HttpContext.Session.SetInt32("PassNum",Count);
+            }
+            string choice = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+            string RP = "";
+            Random rand = new Random();
+            for(short i=0;i<14;i++){
+                int LoD = rand.Next(0,choice.Length);
+                    Console.WriteLine($"LoD: {LoD},char: {choice[LoD]},num: {i+1}");
+                    RP += choice[LoD];
+                }
+            ViewBag.RandomPass = RP;
+            ViewBag.PassNum = HttpContext.Session.GetInt32("PassNum");
+            return View();
+        }
+    }
+}
---
+++ CSharp/RandPassGen/wwwroot/lib/jquery/jquery.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+    $("#button").click(function(res){
+        res.preventDefault();
+        location.reload();
+    })
+})
