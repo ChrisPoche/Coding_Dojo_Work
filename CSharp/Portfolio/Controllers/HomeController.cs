52ae90ccff8218896b408961f12c1d296930b54d
Chris Poche <capoche1@gmail.com>
2018-05-07 20:49:56 -0500

    Mockup portfolio assignment, utilizing routing and static files, specifically css

---
+++ CSharp/Portfolio/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Portfolio.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        [Route("")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        [Route("/projects")]
+        public IActionResult Projects()
+        {
+            return View("Projects");
+        }
+
+
+        [HttpGet]
+        [Route("/contact")]
+        public IActionResult Contact()
+        {
+            return View("Contact");
+        }
+
+    }
+}
